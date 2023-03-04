9137a0929eafe6b4e0022f217fb44b3967c78138
Heric_ <herico_campos@hotmail.com>
2023-03-04 00:56:45 -0300

    Adicionado os states das classes nos btn iniciais

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,37 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
 
 export function Header() {
+  const [btn, setBtn] = useState("a");
+  const valueClass = alterClass(btn);
+
+  function btnSelected(n) {
+    setBtn(n);
+    alterClass(btn);
+  }
+
+  function alterClass(bt) {
+    if (bt === "a") {
+      const newClass = ["btnSelect", "a", "a"];
+      return newClass;
+    }
+    if (bt === "b") {
+      const newClass = ["a", "btnSelect", "a"];
+      return newClass;
+    }
+    if (bt === "c") {
+      const newClass = ["a", "a", "btnSelect"];
+      return newClass;
+    }
+  }
+
   return (
-    <nav className="MenuNav">
-      <div className="MenuLeft">
-        <ul>
-          <li>
-            <Link id="BtnHome" className="link" to="/" onClick={""}>
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link id="BtnProjects" className="link" to="/Projects" onClick={""}>
-              Projects
-            </Link>
-          </li>
-          <li>
-            <Link className="link" to="/InCreation" onClick={""}>
-              In Creation
-            </Link>
-          </li>
-        </ul>
+    <>
+      <div className="logo">
+        <h1>Portfolio</h1>
       </div>
-      <div>
-        <h1>Herico de Lima Campos</h1>
-      </div>
-      <div className="MenuRight">
-        <a href="">Linkedin</a>
-        <a href="">GitHub</a>
-        <a href="">Curriculum</a>
-      </div>
-    </nav>
+      <nav className="MenuNav">
+        <div className="MenuLeft">
+          <ul>
+            <li>
+              <Link
+                id={valueClass[0]}
+                className="btn"
+                to="/"
+                onClick={() => btnSelected("a")}
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                id={valueClass[1]}
+                className="btn"
+                to="/Projects"
+                onClick={() => btnSelected("b")}
+              >
+                Project
+              </Link>
+            </li>
+            <li>
+              <Link
+                id={valueClass[2]}
+                className="btn"
+                to="/InCreation"
+                onClick={() => btnSelected("c")}
+              >
+                In Creation
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        <div className="MenuRight">
+          <a href="">Linkedin</a>
+          <a href="">GitHub</a>
+          <a href="">Curriculum</a>
+        </div>
+      </nav>
+    </>
   );
 }
