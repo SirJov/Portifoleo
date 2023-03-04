278083ca2073c4ec37ad9cfc542d9fe52fc6379d
Heric_ <herico_campos@hotmail.com>
2023-03-04 18:25:01 -0300

    Fragmentado  header, Nav correçao semantca html

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Header } from "./Components/Header/Logo/Header.js";
+import { Nav } from "./Components/Header/Nav.js";
 import { Home } from "./Pages/Home/Home.js";
 import { Projects } from "./Pages/Projects/Projects.js";
-import { Header } from "./Components/Header/Header";
-import "./App.css";
 import { Creating } from "./Pages/InCreation/InCreation.js";
+
+import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
+      <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Projects" element={<Projects />} />
---
+++ src/Components/Header/Logo/Header.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import "./Header.css";
+
+export function Header() {
+  return (
+    <header className="logo">
+      <h1>Portfolio</h1>
+    </header>
+  );
+}
