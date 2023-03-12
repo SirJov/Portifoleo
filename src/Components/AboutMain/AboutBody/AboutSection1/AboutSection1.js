01ac01ae4c755ef6e829c6de32bec1f542577a6b
Heric_ <herico_campos@hotmail.com>
2023-03-11 23:24:27 -0300

    Compon AboutSection1 translate/theme finalizado

--- src/Components/AboutMain/AboutBody/AboutSection1/AboutSection1.js
+++ src/Components/AboutMain/AboutBody/AboutSection1/AboutSection1.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { startTransition } from "react";
+import { useTranslation } from "react-i18next";
 
 import "./AboutSection1.css";
 
@@ -6,18 +7,22 @@ import imgAbout1 from "../../../../assets/imgAbout1.webp";
 import imgAbout2 from "../../../../assets/imgAbout2.webp";
 import imgAbout3 from "../../../../assets/imgAbout3.webp";
 
+import Borda1 from "../../../../assets/Borda1.webp";
+
 export function AboutSection1() {
+  const [t, i18n] = useTranslation();
   return (
     <div className="About1Box">
       <div className="About1SubBox">
         <div>
-          <p>- Olá, meu nome é</p>
+          <p>{t("P1AboutSection1")}</p>
           <br />
-          <h1 className="NickName">Herico de Lima Campos</h1>
+          <h1 className="NickName">{t("H1AboutSection1")}</h1>
           <br />
-          <p></p>
+          <p>{t("P2AboutSection1")}</p>
+          <img className="About1Borda1" src={Borda1} />
         </div>
-        <img src={imgAbout1} />
+        <img className="About1Img1" src={imgAbout1} />
       </div>
     </div>
   );
--- src/Components/Header/TopHeader/OptionsTheme/configTheme/ThemeDark.js
+++ src/Components/Header/TopHeader/OptionsTheme/configTheme/ThemeDark.js
@@ -2,10 +2,13 @@ const Dark = `
 .logo {background-image: linear-gradient(0deg, rgba(0, 25, 27, 0.92), rgb(0, 0, 0));}
 .logo p {color: white;}
 
+.container{color:white;}
+
 .divAbout2 {background-image: linear-gradient(0deg, rgba(1, 11, 12, 0.893), rgb(0, 0, 0),rgba(1, 11, 12, 0.893));}
 .divProject2 {background-image: linear-gradient(0deg, rgba(1, 11, 12, 0.893), rgb(0, 0, 0),rgba(1, 11, 12, 0.893));}
 .divInCreation2 {background-image: linear-gradient(0deg, rgba(1, 11, 12, 0.893), rgb(0, 0, 0),rgba(1, 11, 12, 0.893));}
 
+.About1Borda1 {filter: invert(70%);}
 
 footer {background-image: linear-gradient(0deg, rgb(0, 0, 0), rgba(0, 34, 36, 0.822));}
 footer p {color: white;}
