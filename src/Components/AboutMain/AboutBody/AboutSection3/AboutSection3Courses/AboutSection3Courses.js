b87e1397958b176a7772c6a7d5dfe9cc9c98a6b9
Heric_ <herico_campos@hotmail.com>
2023-03-17 23:25:09 -0300

    Add arrayCourses

--- src/Components/AboutMain/AboutBody/AboutSection3/AboutSection3Courses/AboutSection3Courses.js
+++ src/Components/AboutMain/AboutBody/AboutSection3/AboutSection3Courses/AboutSection3Courses.js
@@ -13,17 +13,21 @@ import Estoquista from "../../../../../assets/Estoquista.webp";
 export function AboutSection3Cards() {
   const [t, i18n] = useTranslation();
 
-  const ArrayExperiencia = [
-    { img: Gesseiro, h5: t("Gesseiro"), msg: t("GesseiroAboutSection3") },
-    { img: Motoboy, h5: t("Motoboy"), msg: t("MotoboyAboutSection3") },
-    { img: Pecuarista, h5: t("Pecuarista"), msg: t("PecuaristaAboutSection3") },
-    { img: Pedreiro, h5: t("Pedreiro"), msg: t("PedreiroAboutSection3") },
+  const ArrayCourses = [
+    { img: Gesseiro, TagA: t(""), msg: t("") },
+    { img: Motoboy, TagA: t(""), msg: t("") },
+    {
+      img: Pecuarista,
+      TagA: t(""),
+      msg: t(""),
+    },
+    { img: Pedreiro, TagA: t(""), msg: t("") },
     {
       img: Carpintaria,
-      h5: t("Carpinteiro"),
-      msg: t("CarpintariaAboutSection3"),
+      h5: t(""),
+      msg: t(""),
     },
-    { img: Estoquista, h5: t("Estoquista"), msg: t("EstoquistaAboutSection3") },
+    { img: Estoquista, h5: t(""), msg: t("") },
   ];
 
   return ArrayExperiencia.map((iten, index) => {
