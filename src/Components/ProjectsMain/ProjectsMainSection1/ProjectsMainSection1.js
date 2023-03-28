9714dfff7a89d1f509ecf85d82826baa5da5c6ea
Heric_ <herico_campos@hotmail.com>
2023-03-27 23:12:56 -0300

    correçao de erros no css Alteraçoes projects

--- src/Components/ProjectsMain/ProjectsMainSection1/ProjectsMainSection1.js
+++ src/Components/ProjectsMain/ProjectsMainSection1/ProjectsMainSection1.js
@@ -1,53 +1,48 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 import "./ProjectsMainSection1.css";
 
 import Borda2L from "../../../assets/AssetsProjects/Borda2L.webp";
 import Borda2R from "../../../assets/AssetsProjects/Borda2R.webp";
 
+import iconFront from "../../../assets/AssetsProjects/iconFront.webp";
+import iconBack from "../../../assets/AssetsProjects/iconBack.webp";
+import iconDidatic from "../../../assets/AssetsProjects/iconDidatic.webp";
+import iconProprio from "../../../assets/AssetsProjects/iconProprio.webp";
+
 export function ProjectsMainSection1() {
+  const [t] = useTranslation();
+
   return (
     <div className="ProjectsMainSection1">
-      <img className="imgBordas" src={Borda2L} />
-      <div>
-        <h3>Projetos</h3>
-        <div className="ProjectsMainSection1Box2">
-          <div>
-            <p>AHSUHEuHASuHE</p>
-          </div>
-          <div>
-            <ul>
-              <li>
-                <img />
-                <p>asdasdddddddddddddd d dd</p>
-              </li>
-              <li>
-                <img />
-                <p>asdasd</p>
-              </li>
-              <li>
-                <img />
-                <p>asdasd</p>
-              </li>
-            </ul>
-            <ul>
-              <li>
-                <img />
-                <p>asdasd</p>
-              </li>
-              <li>
-                <img />
-                <p>asdasd</p>
-              </li>
-              <li>
-                <img />
-                <p>asdasd</p>
-              </li>
-            </ul>
-          </div>
+      <img id="borda1Project" className="imgBordas" src={Borda2L} alt="" />
+      <div className="ProjectsMainSection1Box2">
+        <p>{t("ProjectsMainP1")}</p>
+        <div>
+          <ul>
+            <li>
+              <img className="iconProject" src={iconFront} alt="" />
+              <p>Front-end</p>
+            </li>
+            <li>
+              <img className="iconProject" src={iconProprio} alt="" />
+              <p>{t("iconPerson")}</p>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <img className="iconProject" src={iconBack} alt="" />
+              <p>Back-end</p>
+            </li>
+            <li>
+              <img className="iconProject" src={iconDidatic} alt="" />
+              <p>{t("iconDidatic")}</p>
+            </li>
+          </ul>
         </div>
       </div>
-      <img className="imgBordas" src={Borda2R} />
+      <img id="borda2Project" className="imgBordas" src={Borda2R} alt="" />
     </div>
   );
 }
