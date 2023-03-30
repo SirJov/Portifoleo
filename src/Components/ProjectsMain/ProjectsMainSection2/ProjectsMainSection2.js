409e3b4be583bcdc9a77203be5fab17291762efe
Heric_ <herico_campos@hotmail.com>
2023-03-29 21:16:36 -0300

    Alterando leyout dos cards projectSection2

--- src/Components/ProjectsMain/ProjectsMainSection2/ProjectsMainSection2.js
+++ src/Components/ProjectsMain/ProjectsMainSection2/ProjectsMainSection2.js
@@ -4,21 +4,25 @@ import "./ProjectsMainSection2.css";
 
 import imgNewFrontTasks1 from "../../../assets/AssetsProjects/imgNewFrontTasks1.webp";
 
+import iconFront from "../../../assets/AssetsProjects/iconFront.webp";
+import iconBack from "../../../assets/AssetsProjects/iconBack.webp";
+import iconDidatic from "../../../assets/AssetsProjects/iconDidatic.webp";
+import iconProprio from "../../../assets/AssetsProjects/iconProprio.webp";
+
 export function ProjectsMainSection2() {
   return (
     <div className="ProjectsMainSection2">
       <div className="ProjectsMainSection2Box2">
         <div className="ProjectsMainSection2Box3">
-          <img src={imgNewFrontTasks1} />
+          <img src={imgNewFrontTasks1} alt="" />
           <p>Front React Tasks</p>
         </div>
         <div className="ProjectsMainSection2Box4">
           <p></p>
         </div>
         <div className="ProjectsMainSection2Box5">
-          <img />a
-          <img />a
-          <img />a
+          <img className="iconProjectCards" src={iconFront} alt="" />
+          <img className="iconProjectCards" src={iconProprio} alt="" />
         </div>
       </div>
     </div>
