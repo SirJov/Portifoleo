8f01e63fb7c71b1ef0bb426e7f4c45d8f86d6937
Heric_ <herico_campos@hotmail.com>
2023-03-29 23:11:21 -0300

    Planejando array projetos/ correçoes css responsi

--- src/Components/ProjectsMain/ProjectsMain.js
+++ src/Components/ProjectsMain/ProjectsMain.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useTranslation } from "react-i18next";
+
 
 import { ProjectsMainSection1 } from "./ProjectsMainSection1/ProjectsMainSection1";
 import { ProjectsMainSection2 } from "./ProjectsMainSection2/ProjectsMainSection2";
@@ -7,12 +7,13 @@ import { ProjectsMainSection2 } from "./ProjectsMainSection2/ProjectsMainSection
 import "./ProjectMain.css";
 
 export function ProjectMain() {
-  const [t] = useTranslation();
   return (
     <div className="divProject">
       <div className="divProject2">
         <ProjectsMainSection1 />
-        <ProjectsMainSection2 />
+        <div className="divProjectCards">
+          <ProjectsMainSection2 />
+        </div>
       </div>
     </div>
   );
--- src/Components/ProjectsMain/ProjectsMainSection2/ProjectsMainSection2.js
+++ src/Components/ProjectsMain/ProjectsMainSection2/ProjectsMainSection2.js
@@ -1,15 +1,16 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 import "./ProjectsMainSection2.css";
 
 import imgNewFrontTasks1 from "../../../assets/AssetsProjects/imgNewFrontTasks1.webp";
 
 import iconFront from "../../../assets/AssetsProjects/iconFront.webp";
-import iconBack from "../../../assets/AssetsProjects/iconBack.webp";
-import iconDidatic from "../../../assets/AssetsProjects/iconDidatic.webp";
 import iconProprio from "../../../assets/AssetsProjects/iconProprio.webp";
 
 export function ProjectsMainSection2() {
+  const teste = "iconPerson";
+  const [t] = useTranslation();
   return (
     <div className="ProjectsMainSection2">
       <div className="ProjectsMainSection2Box2">
@@ -21,8 +22,18 @@ export function ProjectsMainSection2() {
           <p></p>
         </div>
         <div className="ProjectsMainSection2Box5">
-          <img className="iconProjectCards" src={iconFront} alt="" />
-          <img className="iconProjectCards" src={iconProprio} alt="" />
+          <img
+            className="iconProjectCards"
+            src={iconFront}
+            title="Front-end"
+            alt=""
+          />
+          <img
+            className="iconProjectCards"
+            title={t(teste)}
+            src={iconProprio}
+            alt=""
+          />
         </div>
       </div>
     </div>
