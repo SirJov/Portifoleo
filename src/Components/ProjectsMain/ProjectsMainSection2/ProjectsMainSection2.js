0567b19d3848c89b006c6dfceed82c9b3405a100
Heric_ <herico_campos@hotmail.com>
2023-04-01 16:36:16 -0300

    Adicionado funcionalidade Modal

--- src/Components/ProjectsMain/ProjectsMainSection2/ProjectsMainSection2.js
+++ src/Components/ProjectsMain/ProjectsMainSection2/ProjectsMainSection2.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
+import { ProjectsModal } from "./ProjectsModal/ProjectsModal";
 
 import "./ProjectsMainSection2.css";
 
@@ -10,32 +11,41 @@ import iconProprio from "../../../assets/AssetsProjects/iconProprio.webp";
 
 export function ProjectsMainSection2() {
   const teste = "iconPerson";
+  const [openModal, setOpenModal] = useState(false);
+
   const [t] = useTranslation();
+
   return (
-    <div className="ProjectsMainSection2">
-      <div className="ProjectsMainSection2Box2">
-        <div className="ProjectsMainSection2Box3">
-          <img src={imgNewFrontTasks1} alt="" />
-          <p>Front React Tasks</p>
-        </div>
-        <div className="ProjectsMainSection2Box4">
-          <p></p>
-        </div>
-        <div className="ProjectsMainSection2Box5">
-          <img
-            className="iconProjectCards"
-            src={iconFront}
-            title="Front-end"
-            alt=""
-          />
-          <img
-            className="iconProjectCards"
-            title={t(teste)}
-            src={iconProprio}
-            alt=""
-          />
+    <>
+      <div className="ProjectsMainSection2">
+        <div
+          className="ProjectsMainSection2Box2"
+          onClick={() => setOpenModal(true)}
+        >
+          <div className="ProjectsMainSection2Box3">
+            <img src={imgNewFrontTasks1} alt="" />
+            <p>Front React Tasks</p>
+          </div>
+          <div className="ProjectsMainSection2Box4">
+            <p></p>
+          </div>
+          <div className="ProjectsMainSection2Box5">
+            <img
+              className="iconProjectCards"
+              src={iconFront}
+              title="Front-end"
+              alt=""
+            />
+            <img
+              className="iconProjectCards"
+              title={t(teste)}
+              src={iconProprio}
+              alt=""
+            />
+          </div>
         </div>
       </div>
-    </div>
+      <ProjectsModal isOpen={openModal} setOpenModal={setOpenModal} />
+    </>
   );
 }
---
+++ src/Components/ProjectsMain/ProjectsMainSection2/ProjectsModal/ProjectsModal.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./ProjectsModal.css";
+
+export function ProjectsModal({ isOpen, setOpenModal }) {
+  if (isOpen)
+    return (
+      <>
+        <div
+          className="ProjectModalBackground"
+          onClick={() => setOpenModal(false)}
+        ></div>
+        <div className="ProjectsModalBox1"></div>
+      </>
+    );
+  return null;
+}
