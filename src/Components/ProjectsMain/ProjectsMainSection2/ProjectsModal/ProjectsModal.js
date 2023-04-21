711208e4aaee0f923a0856b2962af65da05e167c
Heric_ <herico_campos@hotmail.com>
2023-04-20 21:10:31 -0300

    correçoes css projectModal

--- src/Components/ProjectsMain/ProjectsMainSection2/ProjectsModal/ProjectsModal.js
+++ src/Components/ProjectsMain/ProjectsMainSection2/ProjectsModal/ProjectsModal.js
@@ -40,14 +40,7 @@ export function ProjectsModal({ isOpen, objModal, setOpenModal }) {
                   <p>{objModal.usedInProject}</p>
                 </div>
               </div>
-              <div className="modalBox3">
-                <div>
-                  <p>{t(objModal.projectDescriptionGit)}</p>
-                </div>
-                <div>
-                  <p>{t(objModal.projectDescriptionVercel)}</p>
-                </div>
-              </div>
+
               <div className="modalBox4">
                 <BtnLinkGitHub objModal={objModal} />
                 <BtnLinkVercel objModal={objModal} />
